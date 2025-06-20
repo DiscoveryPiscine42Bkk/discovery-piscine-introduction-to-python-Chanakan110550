ccfa4de50d45558c75e43e1dd0f7e5b0ffe61ec1
Chanakan110550 <Chanakanzaa2007@gmail.com>
2025-06-20 08:39:02 +0000

    add42

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,15 @@
+#play_with_arrays-py
+
+def main():
+    original array = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(original_array)
+    
+    #กรองค่า >= 8 แล้วบวก 2
+    transformed = [x + 2 for x in original_array if x >= 8]
+
+    #แปลงเป็นเซตเพื่อลบค่าซ้ำ
+    result_set = set (transformed)
+    print(result_set)
+
+if _name_ _main_ ":
+main_()
